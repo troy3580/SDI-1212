7fddad27afaf00f386e3e6c44d4bdb4d39190649
Troy Stevenson <troy3580@fullsail.edu>
2012-12-04 21:20:36 -0800

    for loop villans

--- stevenson_troy_assignment3/stevenson_troy_assignment3.js
+++ stevenson_troy_assignment3/stevenson_troy_assignment3.js
@@ -22,5 +22,78 @@ for (var key in hero) {
 	console.log("key: " + key + ", value: ", hero[key]); 	
 };
 
+var json = {
+	"villans": {
+		"0001": {
+			"name":  "Lex",
+			"hates": "Superman",
+			"power": "brains"
+			},
+		"0002": {
+			"name":  "Doomsday",
+			"hates": "Superman",
+			"power": "size"
+			},
+		"0003": {
+			"name":  "General Zod",
+			"hates": "Superman",
+			"power": "yellow sun" 
+		}
+	}
+};
+
+for (var key in json.villans) {
+	var villan = json.villans[key]
+};
+
+var json2 = {
+	"friends": [
+		{
+			"id": "0001",
+			"name":  "Lex",
+			"hates": "Superman",
+			"power": "brains"
+		},
+		{
+			"id": "0002",
+			"name":  "Doomsday",
+			"hates": "Superman",
+			"power": "size"
+		},
+		{
+			"id": "0003",
+			"name":  "General Zod",
+			"hates": "Superman",
+			"power": "yellow sun" 
+		}
+	]
+};
+
+var handleData = function(json){
+	for (var i = 0; i < json.vilans.lenth; i++){
+			var villan = json.villans[i];
+			console.log("id: " + villan.id + ", name: " + villan.name + ", hates; " + villan.hates + ", power: " + villan.power); 
+		}
+};
+
+handleData(json2);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
