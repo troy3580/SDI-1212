b3f3bb221689d94a62f539065200ac9bf3f95066
Troy Stevenson <troy3580@fullsail.edu>
2012-12-04 20:21:52 -0800

    spelling errors

--- stevenson_troy_assignment3/stevenson_troy_assignment3.js
+++ stevenson_troy_assignment3/stevenson_troy_assignment3.js
@@ -4,21 +4,23 @@
 // SDI 1212
 // Project 3
 
-var goodGuy = { 
+var hero = { 
 	name: "Superman",
 	power: "Strangth",
 	weakness: "Kriptonite",
-	catchFrase: function () {
+	catchphrase: function () {
 		console.log("I'll protect the people of Earth");
 	}
 };
 
 
 
-console.log(goodGuy);
-goodGuy.catchFrase();
+console.log(hero);
+hero.catchphrase();
 
-for (var key in goodGuy) {
-	console.log("key: " + key + ", value: ", goodGuy.key); 	
-
+for (var key in hero) {
+	console.log("key: " + key + ", value: ", hero[key]); 	
 };
+
+
+
