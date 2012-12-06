9e44d1dd1df2083a334bc6d19a4432c415b85a95
Troy Stevenson <troy3580@fullsail.edu>
2012-12-05 21:28:35 -0800

    Moved things around.
    Fixed bugs.  Tring to make it flow more like a story.

--- stevenson_troy_assignment3/stevenson_troy_assignment3.js
+++ stevenson_troy_assignment3/stevenson_troy_assignment3.js
@@ -4,84 +4,67 @@
 // SDI 1212
 // Project 3
 
+
+
+
 var hero = { 
-	name: "Superman",
-	power: "Strangth",
-	weakness: "Kriptonite",
-	catchphrase: function () {
-		console.log("I'll protect the people of Earth");
+	Hero: "Superman",
+	Power: "Strangth",
+	Weakness: "Kriptonite",
+	Catchphrase: function() {
+		console.log("I'll protect the people of Earth from these evil villans!");
 	}
 };
-
-
 
 console.log(hero);
-hero.catchphrase();
+hero.Catchphrase();
 
-for (var key in hero) {
-	console.log("key: " + key + ", value: ", hero[key]); 	
-};
+console.log("Evil Villan Top Secret Files. ");
 
-var json = {
-	"villans": {
-		"0001": {
-			"name":  "Lex",
-			"hates": "Superman",
-			"power": "brains"
-			},
-		"0002": {
-			"name":  "Doomsday",
-			"hates": "Superman",
-			"power": "size"
-			},
-		"0003": {
-			"name":  "General Zod",
-			"hates": "Superman",
-			"power": "yellow sun" 
-		}
-	}
-};
-
-for (var key in json.villans) {
-	var villan = json.villans[key]
-};
-
-var json2 = {
-	"friends": [
+var json1 = {
+	"villans": [
 		{
-			"id": "0001",
-			"name":  "Lex",
-			"hates": "Superman",
-			"power": "brains"
+			"id":    "0001.",
+			"name":  "Lex.",
+			"hates": "Superman.",
+			"power": "Brains."
 		},
 		{
-			"id": "0002",
-			"name":  "Doomsday",
-			"hates": "Superman",
-			"power": "size"
+			"id":    "0002.",
+			"name":  "Doomsday.",
+			"hates": "Superman.",
+			"power": "Size."
 		},
 		{
-			"id": "0003",
-			"name":  "General Zod",
-			"hates": "Superman",
-			"power": "yellow sun" 
+			"id":    "0003.",
+			"name":  "General Zod.",
+			"hates": "Superman.",
+			"power": "Yellow Sun." 
 		}
 	]
 };
 
-var handleData = function(json){
-	for (var i = 0; i < json.vilans.lenth; i++){
-			var villan = json.villans[i];
-			console.log("id: " + villan.id + ", name: " + villan.name + ", hates; " + villan.hates + ", power: " + villan.power); 
-		}
+var handleData = function(json1) {
+	for (var i = 0; i < json1.villans.length; i++) {
+		var villan = json1.villans[i];
+		console.log("Id: " + villan.id + " Name: " + villan.name + " Hates: " + villan.hates + " Power: " + villan.power);
+	};	
 };
 
-handleData(json2);
+handleData(json1);
 
+var hits = {
+	"name":     "myStrangth",
+	"punches":  30,
+	"kicks":    30,
+	"blasters": 40, 
+	"total" :   function() {
+		var totalDamage = this.punches + this.kicks + this.blasters;
+		return totalDamage;
+	}
+};
 
-
-
-
+console.log("Before they can capture Superman the villans need to hit him " + hits.total() + " times. ");
 
 
 
