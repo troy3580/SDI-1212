ea783ac8f6b4b60255459020dc82783fb3a38723
Troy Stevenson <troy3580@fullsail.edu>
2012-12-06 19:03:31 -0800

    finished what I could.  Ran out of time.
    It gets easyer and easyer every week.  Really starting to get it.

--- stevenson_troy_assignment3/stevenson_troy_assignment3.js
+++ stevenson_troy_assignment3/stevenson_troy_assignment3.js
@@ -92,21 +92,35 @@ caveDepth(893457345734558);
 console.log("feet! ");
 
 var foesBattle = true;
-var hitsLanded = 95;
+var hitsLanded = 105;
 var hitsUsed = ["Punches", "Kicks", "Blasters"];
-//var numbersHit = [25, 25, 45];
+var numbersHit = [25, 25, 45];
+
+console.log("It is " + foesBattle + " that there was a fight between good and evil. ");
 
 var theFight = function () {
-	console.log("It is " + foesBattle + " that there was a fight between good and evil. ");
-	if (foesBattle = true) {
-		console.log("It will be a hard battle for Superman. "); 
-		} else ("Superman will have to go find Lois Lane. ");
-	};
+	if (hitsLanded > totalPower) {
+		console.log("Superman will be captured "); 
+		} else {
+			console.log("Superman will stop the villans and save the day. ");
+			} if (myVillan1 === true) {
+				console.log("Superman can beat Lex. ");
+			} 	else {
+				  console.log("Superman will be in big trouble because " + 
+				              myVillan2 + " and " + myVillan3 + 
+				              "are from Kripton as well. "); 
+		}
+};
 
 theFight();
 
+var bigHits = function () {
+	var hits = ("from " + myVillan2 + "and " + myVillan3 + "hurt Superman the most. ");
+};
 
-
+//I put in an easy 12 hours into this assignment and requested help or guidence at least 3 times through FSO and your gmail email.
+//I had a lot of fun with this one but just don't have enough time in my 10 hour days 6 days a week of work to finish.
+//I did not procrastinate onece either.
 
 
 
